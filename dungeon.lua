b2fd9456c2cf976f1058fb9db9d5cfef73f74d60
Jacob Gustafson <7557867+poikilos@users.noreply.github.com>
2019-06-07 13:37:15 -0400

    Replace use of place_node with add_node

--- dungeon.lua
+++ dungeon.lua
@@ -13,7 +13,7 @@ minetest.register_on_generated(function(minp, maxp, blockseed)
 
 		for i, location in ipairs(locations) do
 			if math.random(3) == 1 then
-				minetest.place_node(location, { name = "loot:loot_node" })
+				minetest.add_node(location, { name = "loot:loot_node" })
 			end
 		end
 end)
